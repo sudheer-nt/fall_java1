a5a53a93895fdfae3ff1235f1d666ee1734f80fc
anveshalluri01 <anveshalluri01@gmail.com>
2022-12-13 12:25:52 -0700

    changes by anvesh

---
+++ src/main/java/hello/Naasproject.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Naasproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("entered Naas Tech");
+
+	}
+
+}
