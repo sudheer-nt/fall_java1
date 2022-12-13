8a74005a6568a6fff63d899fa13c99f24b7a0fb2
nachii <nachikethan888@gmail.com>
2022-12-13 14:23:59 -0700

    nachiabstract class changes

---
+++ src/main/java/abstractclassinterface/nachi/AbstractTest.java
@@ -0,0 +1,12 @@
+package abstractclassinterface.nachi;
+
+public class AbstractTest {
+
+	public static void main(String[] args) {
+		Stud s = new Stud();
+		s.m1();
+		s.student(52,"nachi",24,"CSE");
+
+	}
+
+}
