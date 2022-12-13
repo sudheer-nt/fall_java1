3321ce48004f559476a0e871e6f213aabb7a96e9
Akshathreddy7655 <Akshathreddy7515@gmail.com>
2022-12-13 13:53:30 -0700

    akshath worked on abstactinterface

---
+++ src/main/java/abstractclassinterface/akshath/Rectangle.java
@@ -0,0 +1,16 @@
+package abstractclassinterface.akshath;
+abstract class Shape {
+    int b = 20;
+    abstract public void calculateArea();
+}
+
+public class Rectangle extends Shape {
+    public static void main(String args[]) {
+        Rectangle obj = new Rectangle();
+        obj.b = 200;
+        obj.calculateArea();
+    }
+    public void calculateArea() {
+        System.out.println("Area is " + (b * b));
+    }
+}
