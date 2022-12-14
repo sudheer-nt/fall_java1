03675f6279bef652b1303676c4f5c6d6a96f8db9
anveshalluri01 <anveshalluri01@gmail.com>
2022-12-14 16:20:32 -0700

    overriding is done

---
+++ src/main/java/methodoverriding/anvesh/DisplayOverriding.java
@@ -0,0 +1,14 @@
+package methodoverriding.anvesh;
+public class DisplayOverriding extends Family {
+     public void kids()
+     {
+    	System.out.println("kids are playing"); 
+     }
+	public static void main(String[] args) {
+		DisplayOverriding d=new DisplayOverriding();
+		d.kids();
+		d.parents();
+
+	}
+
+}
