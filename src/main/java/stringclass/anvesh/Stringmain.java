d1bb2a47e45d3a8f7b677f3499e793ad3e1b772d
anveshalluri01 <anveshalluri01@gmail.com>
2022-12-21 23:36:39 -0600

    string class done by anvesh

---
+++ src/main/java/stringclass/anvesh/Stringmain.java
@@ -0,0 +1,15 @@
+package stringclass.anvesh;
+
+public class Stringmain extends stringdemo{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		stringdemo s = new stringdemo();
+		s.display("Hello This is Anvesh");
+		s.display("String Class Demo");
+		s.display("Thank You");
+		
+
+	}
+
+}
