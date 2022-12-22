cb8fe8a57682180882d18c0e951c4072c022b7e0
sudheer-nt <sudheer.fsd9@gmail.com>
2022-12-22 14:48:30 -0700

    objectclass and liststreams topics added

---
+++ src/main/java/collections/liststreams/TestListStreams.java
@@ -0,0 +1,10 @@
+package collections.liststreams;
+
+public class TestListStreams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/objectclass/TestObjectClass.java
@@ -0,0 +1,10 @@
+package objectclass;
+
+public class TestObjectClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
