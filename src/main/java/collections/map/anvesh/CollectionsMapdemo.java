ad7bd65ada55708be25b9e19d0336b23639a7bf2
anveshalluri01 <anveshalluri01@gmail.com>
2022-12-22 21:27:34 -0600

    CollectionsMap Demo

---
+++ src/main/java/collections/map/anvesh/CollectionsMapdemo.java
@@ -0,0 +1,14 @@
+package collections.map.anvesh;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CollectionsMapdemo {
+	void display(HashMap<Integer,String> s) {
+		
+			System.out.println(s);
+			
+		
+	}
+
+}
