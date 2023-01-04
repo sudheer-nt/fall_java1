c3177c1e48d3be6d583c356daaf6c48f6cb25d1f
Akshathreddy7655 <Akshathreddy7515@gmail.com>
2023-01-04 11:59:48 -0700

    substring method is done

---
+++ src/main/java/stringclass/akshath/Bottle.java
@@ -0,0 +1,8 @@
+package stringclass.akshath;
+
+public class Bottle {
+    public static void main (String[]args){
+        String S ="kirkland is the company,water bottle";
+        System.out.println("the Substring is:"+ S.substring(0,8));
+    }
+}
