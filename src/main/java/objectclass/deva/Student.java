ec6372fce5c4ed8a2c2ac312f916c9be6852d6c2
deva <deva@gamil.com>
2023-01-05 10:37:20 -0700

    worked on object class

---
+++ src/main/java/objectclass/deva/Student.java
@@ -0,0 +1,31 @@
+package objectclass.deva;
+public class Student {
+	int rollno;
+	String gender;
+	String course;
+	String exam;
+	
+	public static void main(String[] args) {
+		Student vikky = new Student();
+		//vikky = reference
+		Student dev = new Student();
+		
+		vikky.rollno = 2324;
+		dev.gender = "male";
+		vikky.course = "civil";
+		vikky.exam ="maths";
+		
+		System.out.println("vikky.rollno");
+		
+		System.out.println(dev.gender);
+		
+		System.out.println(dev.exam);
+
+		System.out.println(vikky.course);
+		
+		
+		
+
+	}}
+
+
