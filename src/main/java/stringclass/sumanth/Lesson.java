84b2dc2304af1bdfb4d4c285b480b5d8bd23a810
sumanth <sumanth.work10@gmail.com>
2023-01-13 13:44:56 -0700

    string work done

---
+++ src/main/java/stringclass/sumanth/Lesson.java
@@ -0,0 +1,21 @@
+package stringclass.sumanth;
+
+public class Lesson {
+
+	public static void main(String [] args) {
+		{
+		
+		String java = new String( "Hello Sumanth");
+		String sumanth = new String(" I am from India");
+		
+		System.out.println(java);
+		System.out.println(sumanth); }
+		
+		{
+		String summer = new String(" Hot Weather");
+		String winter = new String(" Its to cool in winter");
+		
+		
+		System.out.println(summer);
+		System.out.println(winter); }}}
+
