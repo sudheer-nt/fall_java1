d4706cf0ab178bae013da74c7e6fa1d451f63697
anveshalluri01 <anveshalluri01@gmail.com>
2023-01-19 11:07:50 -0700

    anvesh collection list

---
+++ src/main/java/collections/list/anvesh/Collectionsmain.java
@@ -0,0 +1,19 @@
+package collections.list.anvesh;
+import java.util.ArrayList;
+
+
+
+public class Collectionsmain extends Collectionsdemo {
+
+	public static void main(String[] args) {
+		Collectionsdemo c = new Collectionsdemo();
+	    ArrayList<String> a = new ArrayList<String>();
+	    a.add("Anvesh");
+	    a.add("Kiran");
+	    a.add("Pranay");
+	    a.add("Shiva");
+	    a.add("Kunal");
+	    a.add("Ravi");
+		c.display(a);
+	}
+}
