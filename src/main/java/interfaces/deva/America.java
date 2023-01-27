a8404454c7d2cb8055439b71834595fbcde2f5f7
deva <deva@gamil.com>
2023-01-27 14:13:58 -0700

    Worked on Collections

---
+++ src/main/java/interfaces/deva/America.java
@@ -0,0 +1,24 @@
+package interfaces.deva;
+
+public class America implements Country {
+@Override
+	public int economyNo() {
+		System.out.println("economyNo is 1");
+		return 1;
+	}
+@Override
+	public String countryName() {
+	System.out.println("America");
+	return null;
+	}
+@Override
+public String countryCapital() {
+		System.out.println("Washington");
+		return null;
+		
+	}
+}
+	
+	
+		
+		
---
+++ src/main/java/interfaces/deva/Country.java
@@ -0,0 +1,8 @@
+package interfaces.deva;
+
+public interface Country {
+	public int economyNo();
+	public String countryName();
+	public String countryCapital();
+	
+	}
