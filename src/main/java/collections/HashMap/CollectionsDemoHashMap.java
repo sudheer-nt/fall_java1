71d046e71c75ee04a50d904b3e5c025f698fb42b
sumanth <sumanth.work10@gmail.com>
2023-02-06 12:14:57 -0700

    HashMap

---
+++ src/main/java/collections/HashMap/CollectionsDemoHashMap.java
@@ -0,0 +1,35 @@
+package collections.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionsDemoHashMap {
+
+	private static char[] i;
+
+	public static void main(String[] args) {
+		
+		Map<String, String> fruits = new HashMap<>();
+		fruits.put("Green","greenapple");
+		fruits.put("Yellow"," Mango");
+		fruits.put("Red","Apple");
+		fruits.put("Blue","Blueberry");
+		
+		
+		System.out.println(fruits.get("Yellow"));
+		System.out.println(fruits.get("Red"));
+		System.out.println(fruits);
+		
+		fruits.containsKey("Green");
+		fruits.containsValue("greenapple");
+		System.out.println(fruits.size());
+		fruits.remove("Green");
+		fruits.replace("blue", "bluegrapes");
+		
+		
+		System.out.println(fruits);
+		
+
+	}
+
+}
